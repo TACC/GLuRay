0cc58292efe8e5574f0d8531347abd63d52b7923
carsonbrownlee <carson@login1.stampede.tacc.utexas.edu>
2016-02-26 17:38:14 -0600

    updating ao samples

--- src/Modules/OptiX/OptiXRenderer.cc
+++ src/Modules/OptiX/OptiXRenderer.cc
@@ -2,17 +2,17 @@
 *                     Copyright (c) 2013-2015 Carson Brownlee
 *         Texas Advanced Computing Center, University of Texas at Austin
 *                       All rights reserved
-* 
+*
 *       This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU Lesser General Public
 * License as published by the Free Software Foundation; either
 * version 2.1 of the License, or (at your option) any later version.
-* 
+*
 * This library is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 * Lesser General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU Lesser General Public
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -171,7 +171,7 @@ void OptiXRenderer::setSize(int w, int h)
   printf("setSize %d %d\n", w,h);
   if (initialized && (w != _width || h != _height))
   {
-    _width = w; _height = h;  
+    _width = w; _height = h;
       if (_bufferMapped)
     {
       buffer->unmap();
@@ -427,7 +427,7 @@ void OptiXRenderer::render()
       // optix::Transform transform = context->createTransform();
       if (!er->transform)
       {
-        er->transform = new optix::Transform; 
+        er->transform = new optix::Transform;
         *(er->transform) = context->createTransform();
       }
       optix::Transform transform = *(er->transform);
@@ -839,7 +839,7 @@ void OptiXRenderer::addRenderable(Renderable* ren)
     material->setClosestHitProgram( 0u, closest_hit );
     material->setAnyHitProgram( 1u, any_hit );
     material["occlusion_distance"]->setFloat(30.0f);
-    material["sqrt_occlusion_samples"]->setInt(4);
+    material["sqrt_occlusion_samples"]->setInt(6);
     #endif
 
 
